a87b206b741c440303c122204b4f63d02cbab241
vibhu8447 <vibhusharma8447@gmail.com>
2021-11-08 06:44:07 +0530

    pricing page added

---
+++ Routes/PricingList.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var router = express.Router();
+router.use(function timeLog (req, res, next) {
+    console.log('Time: ', Date.now())
+    next()
+  })
+
+router.get('/',(req,res)=>{
+    res.send("pricepage");
+})
+module.exports =router;
---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose =require('mongoose');
+const userModel = require("./model/user");
+const PriceList= require('./Routes/PricingList');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const uri = "mongodb+srv://vibhu8447:vibhu@cluster0.elndm.mongodb.net/LEARN1?retryWrites=true&w=majority";
+mongoose
+  .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true})
+  .then(() => console.log("Database connected!"))
+  .catch(err => console.log(err));
+app.use('/pricelist',PriceList);
+  app.post("/add", (request, response) => {
+    console.log(request.body);
+    const user = new userModel(request.body);
+  
+    try {
+       user.save();
+      response.send(user);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+});
+app.listen(3000,()=>{
+console.log("listening to 3000");
+})
