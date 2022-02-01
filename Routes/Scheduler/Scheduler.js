9f1a32c515f89a64c973d33dcab76d39401310a6
Vibhu Sharma <vibhusharma@Vibhus-MacBook-Air.local>
2022-02-01 12:14:32 +0530

    custom recurrence added

--- Routes/Scheduler/Scheduler.js
+++ Routes/Scheduler/Scheduler.js
@@ -11,14 +11,13 @@ router.post('/createCustomerSchedule', (req,res)=>{
     const user_mobile= req.query.user_mobile_No;
     const chat_id= req.query.chat_id;
     const customer_mobile= req.query.mobile;
-    const time=req.query.dateTime;
+    let time=req.query.dateTime;
     const name=req.query.name;
     const img_src=req.query.img_src;
     const message=req.query.message;
     const repeatTime=req.query.repeatTime;
     const status= req.query.status;
-
-    // console.log(id,user_mobile,chat_id,customer_mobile,time,name,img_src,message,repeatTime,status);
+    time=time.substring(0,10)+"T"+time.substring(11,19)+".000Z";
     const schedulerMessage = new SchedulerModel({user_mobile,chat_id,customer_mobile,time,name,img_src,message,repeatTime,status})
   
     try {
@@ -41,24 +40,37 @@ router.get('/getCustomerSchedule?',(req,res)=>{
   })
 
   // to update scheduler message
-router.put('/updateCustomerSchedule',(req,res)=>{
+router.put('/updateCustomerSchedule',async (req,res)=>{
     const id=req.query.scheduledMessageId;
-
-    SchedulerModel.updateOne({_id:`${id}`},req.query)
-    .then((result)=>{
-        res.send({status:true,"result":result});
-    })
-    .catch((err)=>{
-        res.status(401).send(err.message);
-    })
+    const status=req.query.status;
+    const schedulerMessage=await SchedulerModel.findOne({_id:`${id}`});
+    console.log(schedulerMessage.repeatTime);
+    if(schedulerMessage.repeatTime=='0'){
+      SchedulerModel.updateOne({_id:`${id}`},req.query)
+      .then((result)=>{
+          res.send({status:true,"result":result});
+      })
+      .catch((err)=>{
+          res.status(401).send(err.message);
+      })
+    }
+    else if(schedulerMessage.repeatTime!='0'){
+      function addMinutes(date, minutes) {
+        return new Date(date.getTime() + minutes*60000);}
+      let d = new Date(schedulerMessage.time);
+      d=addMinutes(d,parseInt(schedulerMessage.repeatTime));
+      schedulerMessage.time=d.toISOString();
+      schedulerMessage.save();
+      res.send({status:true,message:"time is added"});
+    }
   })
+
   // to delete schuler message 
-  router.delete('/deleteCustomerSchedule',(req,res)=>{
+router.delete('/deleteCustomerSchedule',(req,res)=>{
     const id=req.query.scheduledMessageId;
-    console.log(id);
     SchedulerModel.deleteOne({_id:`${id}`})
     .then((result)=>{
-        res.send({status:true,"result":result});
+        res.send({status:true});
     })
     .catch((err)=>{
         res.status(401).send(err.message);
